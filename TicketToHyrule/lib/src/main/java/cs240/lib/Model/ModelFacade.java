beb57b5c8a4fe1fcc8c8b5cb71e8443528fcb2e9
Unknown <adambguymon@gmail.com>
2018-02-07 10:44:58 -0700

    fixed annoying error

--- TicketToHyrule/lib/src/main/java/cs240/lib/Model/ModelFacade.java
+++ TicketToHyrule/lib/src/main/java/cs240/lib/Model/ModelFacade.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 import cs240.lib.client.ServerProxy;
 import cs240.lib.common.results.JoinResult;
+import cs240.lib.common.results.SignInResult;
 
 /**
  * Created by adam on 2/7/18.
@@ -11,6 +12,7 @@ import cs240.lib.common.results.JoinResult;
 
 public class ModelFacade {
     private ArrayList<Game> gameList = new ArrayList<>();
+    private User currentUser = null;
 
     private static ModelFacade instance;
     private ModelFacade(){}
@@ -21,19 +23,34 @@ public class ModelFacade {
         return instance;
     }
 
+    public String login(String userName, String password){
+        SignInResult signInResult = new SignInResult();
+        currentUser = new User(userName, signInResult.getAuthToken());
+        return null;
+    }
+
     public String joinGame(String userName, String gameName){
         JoinResult result = ServerProxy.SINGLETON.joinGame(userName,gameName);
         if(result.getErrorMessage() != null){
             return result.getErrorMessage();
         }
         else{
-            getGame(gameName).addPlayer();
+            try{
+                getGame(gameName).addPlayer(currentUser);
+                return "";
+            }catch(Exception ex){
+                return "EXCEPTION! " + ex;
+            }
         }
     }
+
     public Game getGame(String gameName){
         for(Game g : gameList){
             if(g.getGameName().equals(gameName)) return g;
         }
         return null;
+    }
+    public ArrayList<Game> getGames(){
+        return gameList;
     }
 }
