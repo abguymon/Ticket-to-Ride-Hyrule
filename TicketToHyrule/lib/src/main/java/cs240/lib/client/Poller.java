91dc1a10c84414644fbc7e7dae1bf9f5068917e4
Camden <35931764+cheeseonyou@users.noreply.github.com>
2018-02-10 13:38:22 -0700

    poller file

---
+++ TicketToHyrule/lib/src/main/java/cs240/lib/client/Poller.java
@@ -0,0 +1,55 @@
+package cs240.lib.client;
+
+import cs240.lib.Model.ModelFacade;
+
+/**
+ * Created by chees on 2/9/2018.
+ */
+
+
+public class Poller implements Runnable {
+    public int commandIndex = 0;
+    public static Poller instance;
+    public static Poller getInstance(){
+        if(instance == null){
+            instance = new Poller();
+        }
+        return instance;
+    }
+    public int getCommandIndex(){return commandIndex;}
+    public void incrementCommandIndex(){commandIndex++;}
+    public void setCommandIndex(int newIndex){commandIndex = newIndex;}
+    //different thread
+    public void run(){ //throws InterruptedException {
+        try {
+            while (true) {
+                fullPoll();
+                Thread.sleep(6000);
+            }
+        }
+        catch (InterruptedException myException){
+            System.out.println("not able to sleep");
+        }
+    }
+
+    public void fullPoll() { //throws InterruptedException {
+        //Statement st = dbConnection.createStatement();
+        //ResultSet rs = st.executeQuery("select * from msg_new_to_bde where ACTION =  804 and SEQ >" + lastSeq + "order by SEQ DESC")
+        ModelFacade.getInstance().pollerCheckServer();
+        //PollerResults myResult =
+        //currentFacade.saveResults(myResult);
+    }
+}
+
+
+/*function doPoll(){
+        $.post('ajax/test.html', function(data) {
+            alert(data);  // process results here
+            setTimeout(doPoll,5000);
+        });
+    }
+while(true){
+    fullPoll();
+    Thread.sleep(10000);
+    }
+*/
