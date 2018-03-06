06937d4e0d99655d97fd0eb5a5649c432895fa6c
David <davidwadams12@gmail.com>
2018-03-05 18:27:06 -0700

    Added skeleton for test button in Map presenter

--- TicketToHyrule/app/src/main/java/cs340/tickettohyrule/PhaseTwoPresenters/MapPresenter.java
+++ TicketToHyrule/app/src/main/java/cs340/tickettohyrule/PhaseTwoPresenters/MapPresenter.java
@@ -15,6 +15,7 @@ import cs340.tickettohyrule.Fragments.MapFragment;
 public class MapPresenter implements Observer {
     private ModelFacade modelFacade = CurrentUserSingleton.getInstance().getModelFacade();
     private MapFragment view = null;
+    private int testNumber = 0;
 
     public void updateHistory(){
 
@@ -33,5 +34,84 @@ public class MapPresenter implements Observer {
 
     public void setView(MapFragment view){
         this.view = view;
+    }
+
+    //test button functions
+    public void runTest(){
+        switch(testNumber){
+            case 0:
+                updatePlayerPoints();
+                break;
+            case 1:
+                addTrainCards();
+                break;
+            case 2:
+                addDestinationCards();
+                break;
+            case 3:
+                updateTrainCards();
+                break;
+            case 4:
+                updateDestinationCards();
+                break;
+            case 5:
+                updateTrainCardDeck();
+                break;
+            case 6:
+                updateDestinationCardDeck();
+                break;
+            case 7:
+                claimRoute();
+                break;
+            case 8:
+                chat();
+                break;
+            case 9:
+                addToGameHistory();
+                break;
+        }
+        if (testNumber > 9) {
+            testNumber++;
+        }else if (testNumber == 9){
+            testNumber = 0;
+        }
+    }
+
+    private void addToGameHistory() {
+
+    }
+
+    private void chat() {
+
+    }
+
+    private void claimRoute() {
+
+    }
+
+    private void updateDestinationCardDeck() {
+
+    }
+
+    private void updateTrainCardDeck() {
+
+    }
+
+    private void updateDestinationCards() {
+
+    }
+
+    private void updateTrainCards() {
+
+    }
+
+    private void addDestinationCards() {
+
+    }
+    private void addTrainCards() {
+
+    }
+    private void updatePlayerPoints(){
+
     }
 }
