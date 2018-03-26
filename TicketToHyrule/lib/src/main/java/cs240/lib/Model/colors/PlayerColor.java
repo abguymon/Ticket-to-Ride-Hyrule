35a4eb681c61b6b3694f2a1d37b43731152b8ee9
Philip Stanley <philip.stanley12@gmail.com>
2018-03-25 20:57:24 -0600

    black to red because native americans are better than african americans

--- TicketToHyrule/lib/src/main/java/cs240/lib/Model/colors/PlayerColor.java
+++ TicketToHyrule/lib/src/main/java/cs240/lib/Model/colors/PlayerColor.java
@@ -4,4 +4,4 @@ package cs240.lib.Model.colors;
  * Created by David on 2/21/2018.
  */
 
-public enum PlayerColor {GREEN, BLACK, PINK, YELLOW, BLUE}//for Link, Gannon, Zelda, Goron, Zora respectively
+public enum PlayerColor {GREEN, RED, PINK, YELLOW, BLUE}//for Link, Gannon, Zelda, Goron, Zora respectively
