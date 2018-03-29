49b776b039420138ee7dbf5d5489540930a59c4f
Philip Stanley <philip.stanley12@gmail.com>
2018-03-28 21:47:32 -0600

    route equals fixed

--- TicketToHyrule/lib/src/main/java/cs240/lib/Model/gameParts/Route.java
+++ TicketToHyrule/lib/src/main/java/cs240/lib/Model/gameParts/Route.java
@@ -76,7 +76,7 @@ public class Route {
                 if (this.cityNodes.getCity1().getCityName().equals(route.getCityNodes().getCity1().getCityName())) {
                     if (this.cityNodes.getCity2().getCityName().equals(route.getCityNodes().getCity2().getCityName())) {
                         if (this.isClaimed() == route.isClaimed()) {
-                            if (this.owner.equals(route.getOwner()))
+                            //if (this.owner.equals(route.getOwner()))
                             return true;
                         }
                     }
