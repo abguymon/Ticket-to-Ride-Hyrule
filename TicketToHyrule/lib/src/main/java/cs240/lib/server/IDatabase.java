8a7b8465ac74a60212b4f8328e0b59f46a9ac85c
David <davidwadams12@gmail.com>
2018-04-11 14:45:06 -0600

    Added function to IDatabase

--- TicketToHyrule/lib/src/main/java/cs240/lib/server/IDatabase.java
+++ TicketToHyrule/lib/src/main/java/cs240/lib/server/IDatabase.java
@@ -3,11 +3,12 @@ package cs240.lib.server;
 /**
  * Created by David on 4/7/2018.
  */
-//TODO: how to put these classes in a jar file?
 public interface IDatabase {
     boolean create(Object object);
     boolean insert(Object object);
     Object read(String toRead);
+    Object[] readAll(String toRead);
     boolean update(Object object);
     boolean delete(Object object);
+    void clear();
 }
